e86d2879c8092f02abf68e072d291601258e8d07
Ismaël Guerrib <ismael.guerrib@hotmail.fr>
2020-09-10 17:54:02 +0200

    second commit

--- src/App.js
+++ src/App.js
@@ -3,7 +3,18 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <div className="container">
+      <div className="row">
+        <div className="col-sm-6">
+          <textarea className="form-control" rows="35">
+          </textarea>
+        </div>
+        <div className="col-sm-6">
+          <h1>Résultat</h1>
+        </div>
+      </div>
+
+
 
     </div>
   );
